ea903c73e9daee941846aca3ac609fac6014cd80
XiaoLi_1456 <XiaoLi_1456@outlook.com>
2022-08-04 23:58:20 +0800

    fix: 修改card 组件一些存在的问题

---
+++ components/PCard/card.ts
@@ -0,0 +1,30 @@
+export const Props = {
+  isAside: {
+    type: Boolean,
+    default: false
+  },
+  avatar: {
+    type: String,
+    default: (): string => ''
+  },
+  image: {
+    type: String,
+    default: (): string => ''
+  },
+  padding: {
+    type: String,
+    default: (): string => ''
+  },
+  border: {
+    type: String,
+    default: (): string => ''
+  },
+  shadow: {
+    type: Boolean,
+    default: false
+  },
+  angle: {
+    type: Boolean,
+    default: false
+  }
+}
---
+++ components/PCard/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="classList" :style="{ padding }">
+    <div v-if="isAside || image" class="card-aside">
+      <el-image :src="image" fit="contain" />
+    </div>
+    <div class="p-card-main">
+      <div class="p-card-header">
+        <div class="avatar">
+          <el-image />
+        </div>
+        <slot name="header" />
+      </div>
+      <div class="p-card-content">
+        <slot />
+      </div>
+      <div class="p-card-bottom">
+        <slot name="bottom" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" name="PCard">
+  import { ElImage } from 'element-plus'
+  import type { ComputedRef } from 'vue'
+  import { Props } from './card'
+  import { computed } from 'vue'
+
+  const props = defineProps(Props)
+
+  const classList: ComputedRef<object | string[]> = computed((): object | string[] => {
+    const { isAside, avatar, image, padding, border, shadow, angle } = props
+
+    return [
+      'p-card',
+      {
+        'p-card-shadow': shadow,
+        'p-card-angle': angle
+      }
+    ]
+  })
+</script>
+
+<style scoped lang="scss">
+  @import './index.scss';
+</style>
